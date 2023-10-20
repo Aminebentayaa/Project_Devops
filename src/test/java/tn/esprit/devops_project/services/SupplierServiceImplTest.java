18d404b803577e52ce00bc12738035ab8f7d6eb8
Aminebentayaa <aminebentayaa04@gmail.com>
2023-10-20 23:33:05 +0100

    update jenkinsfile

--- src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/SupplierServiceImplTest.java
@@ -2,12 +2,10 @@ package tn.esprit.devops_project.services;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.When;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
